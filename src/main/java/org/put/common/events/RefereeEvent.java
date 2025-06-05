bb6ff4e8f835109b244e098e6e0f5fe00ffba832
Maxence CHUTAUX <darksas924@gmail.com>
2025-06-05 09:36:57 +0200

    push of complete of referee event

--- src/main/java/org/put/common/events/RefereeEvent.java
+++ src/main/java/org/put/common/events/RefereeEvent.java
@@ -7,10 +7,21 @@ import org.put.common.match.Result;
 import java.security.Timestamp;
 
 public class RefereeEvent extends Event{
-    private Event TrigerEvent;
+    private Event TriggerEvent;
     private Event FollowingEvent;
 
-    public RefereeEvent(Timestamp timestamp, String matchpart, Contestant contestant, int typeEvent, Contest contest, Result result) {
+    public RefereeEvent(Timestamp timestamp, String matchpart, Contestant contestant, int typeEvent, Contest contest, Result result,Event Trigger) {
         super(timestamp, matchpart, contestant, typeEvent, contest, result);
+        TriggerEvent = Trigger;
     }
+    public Event GetTriggerEvent(){
+        return TriggerEvent;
+    }
+    public Event GetFollowingEvent(){
+        return FollowingEvent;
+    }
+    public void SetFollowingEvent(Event Following){
+        FollowingEvent = Following;
+    }
+    
 }
